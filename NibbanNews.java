346fd40120484f88c79f71b1f9d9e8b1421def70
Rishabh <thoratrishabh28@gmail.com>
2022-08-26 17:18:44 +0530

    commit

---
+++ NibbanNews.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.nibbannews;
+
+/**
+ *
+ * @author Admin
+ */
+public class NibbanNews {
+
+    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+        Portals portal = new Portals();
+        portal.setVisible(true);
+    }
+}
